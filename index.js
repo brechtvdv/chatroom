21ba41f3e83e0532ba11ddc3cfbd708a6dc998f7
brechtvdv <br5cht@hotmail.com>
2015-04-11 19:41:18 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+    socket.on('disconnect', function(){
+        console.log('User got disconnected');
+        io.emit('chat message', 'user just got disconnected');
+        });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
