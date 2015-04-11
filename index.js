e651d7d74a6b277f51b09298c6d7525a87fe775c
brechtvdv <br5cht@hotmail.com>
2015-04-11 20:31:03 +0200

    Resolves #2

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-    io.emit('chat message', 'user joined the chatroom');
+    socket.on('name', function(nickname){
+      console.log(nickname + ' has connected');
+      io.emit('chat message', nickname + ' just joined the chatroom.');
+    });
     socket.on('disconnect', function(){
-        console.log('User got disconnected');
+        console.log('user just got disconnected');
         io.emit('chat message', 'user just got disconnected');
         });
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', function(data){
+        console.log(data.nickname + ': ' + data.msg);
+        io.emit('chat message', data.nickname + ": " + data.msg);
     });
 });
